d032b9a03811140e1fc81e46d145a9d957ea0ce4
Dastan <kanatdastan56@gmail.com>
2025-12-21 03:52:45 +0500

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Envelope from "./Envelope";
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Envelope />
+  </React.StrictMode>
+);
